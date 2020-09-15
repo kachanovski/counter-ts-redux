67cefc52fe2fc20b3e9b6f9e68b9dc93676956fd
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-09-15 16:39:36 +0300

    Updates

---
+++ precache-manifest.f49a598c3fafc134b42dcd43781e7596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81dae288bc8908f425869d7e9926d557",
+    "url": "/counter-ts-redux/index.html"
+  },
+  {
+    "revision": "a76b5c4c8d9143379906",
+    "url": "/counter-ts-redux/static/css/main.cf9e3b89.chunk.css"
+  },
+  {
+    "revision": "fe6db2cd8ae4ec63c034",
+    "url": "/counter-ts-redux/static/js/2.7a949146.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter-ts-redux/static/js/2.7a949146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76b5c4c8d9143379906",
+    "url": "/counter-ts-redux/static/js/main.81f7bc5e.chunk.js"
+  },
+  {
+    "revision": "44638b7da75a11171f38",
+    "url": "/counter-ts-redux/static/js/runtime-main.80bea120.js"
+  }
+]);
